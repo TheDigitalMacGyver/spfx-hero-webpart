4406576ffd611eacce3e53599213f07f2dbe19c4
Scott Ellis "Digital MacGyver <39521790+TheDigitalMacGyver@users.noreply.github.com>
2025-06-30 11:55:36 -0700

    Create HeroWebPart.ts

---
+++ src/webparts/hero/HeroWebPart.ts
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import Hero from './components/Hero';
+import { IHeroProps } from './components/IHeroProps';
+
+export interface IHeroWebPartProps {
+  title: string;
+}
+
+export default class HeroWebPart extends BaseClientSideWebPart<IHeroWebPartProps> {
+  public render(): void {
+    const element: React.ReactElement<IHeroProps> = React.createElement(
+      Hero,
+      { title: this.properties.title }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+}
